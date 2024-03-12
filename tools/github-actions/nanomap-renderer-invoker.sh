d9b5ecfa671f019d4e0988ce3d33679042ec16c0
Cadyn Bombaci <cadynspacetechguy@gmail.com>
2024-03-12 16:38:44 -0700

    fix nanomap shenanigans

--- tools/github-actions/nanomap-renderer-invoker.sh
+++ tools/github-actions/nanomap-renderer-invoker.sh
@@ -19,8 +19,6 @@ map_files=(
 # Move and rename files so the game understands them
 cd "data/minimaps"
 
-convert "talon1_nanomap_z1.png" -resize 2240x2240 "tether_nanomap_z13.png"
-convert "talon2_nanomap_z1.png" -resize 2240x2240 "tether_nanomap_z14.png"
 convert "southern_cross-1-1.png" -resize 2240x2240 "southern_cross_nanomap_z1.png"
 convert "southern_cross-2-1.png" -resize 2240x2240 "southern_cross_nanomap_z2.png"
 convert "southern_cross-3-1.png" -resize 2240x2240 "southern_cross_nanomap_z3.png"
@@ -32,6 +30,18 @@ convert "southern_cross-8-1.png" -resize 2240x2240 "southern_cross_nanomap_z8.pn
 convert "southern_cross-9-1.png" -resize 2240x2240 "southern_cross_nanomap_z9.png"
 convert "southern_cross-10-1.png" -resize 2240x2240 "southern_cross_nanomap_z10.png"
 convert "southern_cross-11-1.png" -resize 2240x2240 "southern_cross_nanomap_z11.png"
+
+rm -rf "southern_cross-1-1.png"
+rm -rf "southern_cross-2-1.png"
+rm -rf "southern_cross-3-1.png"
+rm -rf "southern_cross-4-1.png"
+rm -rf "southern_cross-5-1.png"
+rm -rf "southern_cross-6-1.png"
+rm -rf "southern_cross-7-1.png"
+rm -rf "southern_cross-8-1.png"
+rm -rf "southern_cross-9-1.png"
+rm -rf "southern_cross-10-1.png"
+rm -rf "southern_cross-11-1.png"
 
 cd "../../"
 cp data/minimaps/* "icons/_nanomaps/"
