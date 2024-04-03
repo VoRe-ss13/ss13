45f74e914e53b36b79552fc7a4afd44abcb9e15e
Cadyn <35672377+cadyn@users.noreply.github.com>
2024-04-03 15:08:01 -0700

    Update build-testing.ps1

--- tools/build/build-testing.ps1
+++ tools/build/build-testing.ps1
@@ -1,6 +1,6 @@
 Set-Variable -Name "basedir" -Value "$PSScriptRoot/../.."
 
-(Get-Content "$basedir/vorestation.dme").Replace('#include "maps\southern_cross\southern_cross.dm"', '#include "maps\virgo_minitest\virgo_minitest.dm"') | Set-Content "$basedir/vorestation.dme"
+(Get-Content "$basedir/vorestation.dme").Replace('#include "maps\relic_base\relicbase.dm"', '#include "maps\virgo_minitest\virgo_minitest.dm"') | Set-Content "$basedir/vorestation.dme"
 & "$basedir/tools/build/build.bat"
-(Get-Content "$basedir/vorestation.dme").Replace('#include "maps\virgo_minitest\virgo_minitest.dm"', '#include "maps\southern_cross\southern_cross.dm"') | Set-Content "$basedir/vorestation.dme"
+(Get-Content "$basedir/vorestation.dme").Replace('#include "maps\virgo_minitest\virgo_minitest.dm"', '#include "maps\relic_base\relicbase.dm"') | Set-Content "$basedir/vorestation.dme"
 Read-Host -Prompt "Press any key to continue"
