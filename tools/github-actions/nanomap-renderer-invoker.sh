00c2d0003de5f964c63d8db058d4aed48d8e5abf
Kashargul <144968721+Kashargul@users.noreply.github.com>
2024-11-21 23:56:01 +0100

    enbale relic base rendering

--- tools/github-actions/nanomap-renderer-invoker.sh
+++ tools/github-actions/nanomap-renderer-invoker.sh
@@ -2,7 +2,13 @@
 BASEDIR=$PWD
 #Put directories to get maps from here. One per line.
 mapdirs=(
+<<<<<<< HEAD
     "maps/relic_base"
+=======
+    "modular_chomp/maps/southern_cross"
+    "modular_chomp/maps/soluna_nexus"
+    "modular_chomp/maps/relic_base"
+>>>>>>> 24009047fc (enbale relic base rendering (#9514))
 )
 #DO NOT TOUCH THIS VARIABLE. It will automatically fill with any maps in mapdirs that are form MAPNAME-n.dmm where n is the z level.
 map_files=()
