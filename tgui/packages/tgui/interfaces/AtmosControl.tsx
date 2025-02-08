13295b8dc181ff6ab94e9386b69ce5f900a6c0aa
CHOMPStation2StaffMirrorBot <94713762+CHOMPStation2StaffMirrorBot@users.noreply.github.com>
2025-02-08 12:24:15 -0700

    [MIRROR] rework atmos control

--- tgui/packages/tgui/interfaces/AtmosControl.tsx
+++ tgui/packages/tgui/interfaces/AtmosControl.tsx
@@ -1,9 +1,16 @@
 import { sortBy } from 'common/collections';
 import { useState } from 'react';
+<<<<<<< HEAD
 
 import { useBackend } from '../backend';
 import { Box, Button, Icon, NanoMap, Section, Tabs } from '../components';
 import { Window } from '../layouts';
+=======
+import { useBackend } from 'tgui/backend';
+import { NanoMap } from 'tgui/components';
+import { Window } from 'tgui/layouts';
+import { Box, Button, Icon, Section, Stack, Tabs } from 'tgui-core/components';
+>>>>>>> ab6a02b150 ([MIRROR] rework atmos control (#10107))
 
 type alarm = {
   name: string;
@@ -22,7 +29,7 @@ type Data = {
 export const AtmosControl = (props) => {
   return (
     <Window width={600} height={440}>
-      <Window.Content scrollable>
+      <Window.Content>
         <AtmosControlContent />
       </Window.Content>
     </Window>
@@ -39,6 +46,7 @@ export const AtmosControlContent = (props) => {
   const [tabIndex, setTabIndex] = useState(0);
   const [zoom, setZoom] = useState(1);
 
+<<<<<<< HEAD
   let body;
   // Alarms View
   if (tabIndex === 0) {
@@ -82,26 +90,72 @@ export const AtmosControlContent = (props) => {
       </Box>
     );
   }
+=======
+  const tab: React.JSX.Element[] = [];
+
+  tab[0] = (
+    <Section title="Alarms" scrollable fill>
+      {alarms.map((alarm) => (
+        <Button
+          key={alarm.name}
+          color={
+            alarm.danger === 2 ? 'bad' : alarm.danger === 1 ? 'average' : ''
+          }
+          onClick={() => act('alarm', { alarm: alarm.ref })}
+        >
+          {alarm.name}
+        </Button>
+      ))}
+    </Section>
+  );
+  // Please note, if you ever change the zoom values,
+  // you MUST update styles/components/Tooltip.scss
+  // and change the @for scss to match.
+  tab[1] = (
+    <Box height="526px" mb="0.5rem" overflow="hidden">
+      <NanoMap zoomScale={data.zoomScale} onZoom={(v) => setZoom(v)}>
+        {alarms
+          .filter((x) => ~~x.z === ~~config.mapZLevel)
+          .map((cm) => (
+            <NanoMap.Marker
+              key={cm.ref}
+              x={cm.x}
+              y={cm.y}
+              zoom={zoom}
+              icon="bell"
+              tooltip={cm.name}
+              color={cm.danger ? 'red' : 'green'}
+              onClick={() => act('alarm', { alarm: cm.ref })}
+            />
+          ))}
+      </NanoMap>
+    </Box>
+  );
+>>>>>>> ab6a02b150 ([MIRROR] rework atmos control (#10107))
 
   return (
-    <>
-      <Tabs>
-        <Tabs.Tab
-          key="AlarmView"
-          selected={0 === tabIndex}
-          onClick={() => setTabIndex(0)}
-        >
-          <Icon name="table" /> Alarm View
-        </Tabs.Tab>
-        <Tabs.Tab
-          key="MapView"
-          selected={1 === tabIndex}
-          onClick={() => setTabIndex(1)}
-        >
-          <Icon name="map-marked-alt" /> Map View
-        </Tabs.Tab>
-      </Tabs>
-      <Box m={2}>{body}</Box>
-    </>
+    <Stack fill vertical>
+      <Stack.Item>
+        <Tabs>
+          <Tabs.Tab
+            key="AlarmView"
+            selected={0 === tabIndex}
+            onClick={() => setTabIndex(0)}
+          >
+            <Icon name="table" /> Alarm View
+          </Tabs.Tab>
+          <Tabs.Tab
+            key="MapView"
+            selected={1 === tabIndex}
+            onClick={() => setTabIndex(1)}
+          >
+            <Icon name="map-marked-alt" /> Map View
+          </Tabs.Tab>
+        </Tabs>
+      </Stack.Item>
+      <Stack.Item grow m={2}>
+        {tab[tabIndex]}
+      </Stack.Item>
+    </Stack>
   );
 };
