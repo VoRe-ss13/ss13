65a8f8f793cb61e8033a00dab24b8f8d5133d498
CHOMPStation2StaffMirrorBot <94713762+CHOMPStation2StaffMirrorBot@users.noreply.github.com>
2025-04-26 12:26:32 -0700

    [MIRROR] More RIG Intros

--- tgui/packages/tgui/interfaces/RIGSuit/RIGSuitHardware.tsx
+++ tgui/packages/tgui/interfaces/RIGSuit/RIGSuitHardware.tsx
@@ -37,6 +37,7 @@ export const RIGSuitHardware = (props) => {
             </Button>
           }
         >
+<<<<<<< HEAD
           {helmet ? capitalize(helmet) : 'ERROR'}
         </LabeledList.Item>
         <LabeledList.Item
@@ -88,3 +89,160 @@ export const RIGSuitHardware = (props) => {
     </Section>
   );
 };
+=======
+          &nbsp;
+        </Stack.Item>
+        <Stack.Item grow>
+          <Stack fill align="center" justify="center">
+            <Stack.Item>
+              <Box
+                className="RIGSuit__FadeIn"
+                style={{
+                  display: 'grid',
+                  gridTemplateColumns: '1fr 1fr',
+                  gap: '10px',
+                  width: 'fit-content',
+                }}
+              >
+                <Tooltip
+                  content={
+                    'Suit Seals: ' + sealing
+                      ? 'Sealing'
+                      : sealed
+                        ? 'Sealed'
+                        : 'UNSEALED'
+                  }
+                >
+                  <Box
+                    position="relative"
+                    className="RIGSuit__Icon"
+                    style={{
+                      filter: sealing
+                        ? 'grayscale(100%) brightness(0.5)'
+                        : sealed
+                          ? undefined
+                          : 'grayscale(80%)',
+                    }}
+                    onClick={() => act('toggle_seals')}
+                  >
+                    <DmIcon
+                      height={6}
+                      width={6}
+                      icon="icons/hud/rig/rig_ui_slots.dmi"
+                      icon_state="base"
+                    />
+                    <Box position="absolute" top={0.5} left={0.5}>
+                      <Icon name="power-off" size={5} color="#5c9fd8" />
+                    </Box>
+                  </Box>
+                </Tooltip>
+
+                <Tooltip
+                  content={
+                    aioverride
+                      ? 'Toggle AI Control Off'
+                      : 'Toggle AI Control On'
+                  }
+                >
+                  <Box
+                    position="relative"
+                    className="RIGSuit__Icon"
+                    style={{
+                      filter: aioverride ? undefined : 'grayscale(80%)',
+                    }}
+                    onClick={() => act('toggle_ai_control')}
+                  >
+                    <DmIcon
+                      height={6}
+                      width={6}
+                      icon="icons/hud/rig/rig_ui_slots.dmi"
+                      icon_state="base"
+                    />
+                    <Box position="absolute" top={0.7} left={0.5}>
+                      <Icon name="robot" size={4} color="#5c9fd8" />
+                    </Box>
+                  </Box>
+                </Tooltip>
+
+                <Tooltip content={cooling ? 'Cooling: On' : 'Cooling: Off'}>
+                  <Box
+                    position="relative"
+                    className="RIGSuit__Icon"
+                    style={{
+                      filter: cooling ? undefined : 'grayscale(80%)',
+                    }}
+                    onClick={() => act('toggle_cooling')}
+                  >
+                    <DmIcon
+                      height={6}
+                      width={6}
+                      icon="icons/hud/rig/rig_ui_slots.dmi"
+                      icon_state="base"
+                    />
+                    <Box position="absolute" top={1} left={1}>
+                      <Icon name="wind" size={4} color="#5c9fd8" />
+                    </Box>
+                  </Box>
+                </Tooltip>
+
+                <Tooltip content="Tank Settings">
+                  <Box
+                    position="relative"
+                    className="RIGSuit__Icon"
+                    onClick={() => act('tank_settings')}
+                  >
+                    <DmIcon
+                      height={6}
+                      width={6}
+                      icon="icons/hud/rig/rig_ui_slots.dmi"
+                      icon_state="base"
+                    />
+                    <Box position="absolute" top={1} left={0.5}>
+                      <Icon name="lungs" size={4} color="#5c9fd8" />
+                    </Box>
+                  </Box>
+                </Tooltip>
+              </Box>
+            </Stack.Item>
+          </Stack>
+        </Stack.Item>
+      </Stack>
+    </Section>
+  );
+};
+
+const HardwarePiece = (props: {
+  name: string;
+  icon: string;
+  selected: BooleanLike;
+  disabled: BooleanLike;
+  onClick: () => void;
+}) => {
+  let filter;
+  if (props.disabled) {
+    filter = 'grayscale(100%) brightness(0.5)';
+  } else if (props.selected) {
+    filter = undefined;
+  } else {
+    filter = 'grayscale(80%)';
+  }
+
+  return (
+    <Tooltip content={capitalize(props.name) || 'ERROR'}>
+      <Box className="RIGSuit__Icon">
+        <DmIcon
+          height={6}
+          width={6}
+          icon="icons/hud/rig/rig_ui_slots.dmi"
+          icon_state={props.icon}
+          style={{
+            cursor: 'pointer',
+            filter,
+          }}
+          onClick={props.onClick}
+        />
+      </Box>
+    </Tooltip>
+  );
+};
+>>>>>>> 075b9a84a7 ([MIRROR] More RIG Intros (#10761))
