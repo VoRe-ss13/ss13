7d10be069a6cc3b97f9f154f6e0369ab3a2d6daa
CHOMPStation2StaffMirrorBot <94713762+CHOMPStation2StaffMirrorBot@users.noreply.github.com>
2025-06-29 06:18:34 -0700

    [MIRROR] removes tgui sonar, dev server oversights

--- tgui/packages/tgui-dev-server/index.ts
+++ tgui/packages/tgui-dev-server/index.ts
@@ -4,18 +4,31 @@
  * @license MIT
  */
 
+<<<<<<< HEAD:tgui/packages/tgui-dev-server/index.js
 import { reloadByondCache } from './reloader.js';
 import { createCompiler } from './webpack.js';
+=======
+import fs from 'node:fs';
+
+import { reloadByondCache } from './reloader';
+import { createCompiler } from './webpack';
+>>>>>>> 8a4f06eed4 ([MIRROR] removes tgui sonar, dev server oversights (#11129)):tgui/packages/tgui-dev-server/index.ts
 
 const noHot = process.argv.includes('--no-hot');
 const noTmp = process.argv.includes('--no-tmp');
 const reloadOnce = process.argv.includes('--reload');
 
 async function setupServer() {
+<<<<<<< HEAD:tgui/packages/tgui-dev-server/index.js
   const compiler = await createCompiler({
     hot: !noHot,
     useTmpFolder: !noTmp,
   });
+=======
+  fs.mkdirSync('./public/.tmp', { recursive: true });
+
+  const compiler = await createCompiler({ mode: 'development', hot: true });
+>>>>>>> 8a4f06eed4 ([MIRROR] removes tgui sonar, dev server oversights (#11129)):tgui/packages/tgui-dev-server/index.ts
 
   // Reload cache once
   if (reloadOnce) {
--- tgui/packages/tgui-dev-server/webpack.ts
+++ tgui/packages/tgui-dev-server/webpack.ts
@@ -4,16 +4,22 @@
  * @license MIT
  */
 
+<<<<<<< HEAD:tgui/packages/tgui-dev-server/webpack.js
 import fs from 'node:fs';
 import { createRequire } from 'node:module';
+=======
+import { createRequire } from 'module';
+>>>>>>> 8a4f06eed4 ([MIRROR] removes tgui sonar, dev server oversights (#11129)):tgui/packages/tgui-dev-server/webpack.ts
 
-import { loadSourceMaps, setupLink } from './link/server.js';
+import { loadSourceMaps } from './link/retrace';
+import { broadcastMessage, setupLink } from './link/server';
 import { createLogger } from './logging.js';
 import { reloadByondCache } from './reloader.js';
-import { resolveGlob } from './util.js';
+import { resolveGlob } from './util';
 
 const logger = createLogger('rspack');
 
+<<<<<<< HEAD:tgui/packages/tgui-dev-server/webpack.js
 /**
  * @param {any} config
  * @return {RspackCompiler}
@@ -40,31 +46,70 @@ class RspackCompiler {
     const devConfig = createDevConfig({}, options);
 
     const mergedConfig = { ...config, ...devConfig };
+=======
+export async function createCompiler(
+  options: Record<string, any>,
+): Promise<WebpackCompiler> {
+  const compiler = new WebpackCompiler();
+  await compiler.setup(options);
+
+  return compiler;
+}
+
+// eslint-disable-next-line @typescript-eslint/consistent-type-imports
+type WebpackImport = typeof import('webpack');
+
+class WebpackCompiler {
+  public webpack: WebpackImport;
+  public config: Record<string, any>;
+  public bundleDir: string;
+
+  async setup(options: Record<string, any>): Promise<void> {
+    // Create a require context that is relative to project root
+    // and retrieve all necessary dependencies.
+    const requireFromRoot = createRequire(import.meta.dirname + '/../../..');
+    const webpack: WebpackImport = await requireFromRoot('webpack');
+
+    const createConfig = await requireFromRoot('./webpack.config.js');
+    const config = createConfig({}, options);
+>>>>>>> 8a4f06eed4 ([MIRROR] removes tgui sonar, dev server oversights (#11129)):tgui/packages/tgui-dev-server/webpack.ts
 
     // Inject the HMR plugin into the config if we're using it
     if (options.hot) {
       mergedConfig.plugins.push(new rspack.HotModuleReplacementPlugin());
     }
+<<<<<<< HEAD:tgui/packages/tgui-dev-server/webpack.js
     this.rspack = rspack;
     this.config = mergedConfig;
+=======
+
+    this.webpack = webpack;
+    this.config = config;
+>>>>>>> 8a4f06eed4 ([MIRROR] removes tgui sonar, dev server oversights (#11129)):tgui/packages/tgui-dev-server/webpack.ts
     this.bundleDir = config.output.path;
   }
 
-  async watch() {
+  async watch(): Promise<void> {
     logger.log('setting up');
     // Setup link
     const link = setupLink();
     // Instantiate the compiler
+<<<<<<< HEAD:tgui/packages/tgui-dev-server/webpack.js
     const compiler = this.rspack.rspack(this.config);
+=======
+    const compiler = this.webpack.webpack(this.config);
+
+>>>>>>> 8a4f06eed4 ([MIRROR] removes tgui sonar, dev server oversights (#11129)):tgui/packages/tgui-dev-server/webpack.ts
     // Clear garbage before compiling
     compiler.hooks.watchRun.tapPromise('tgui-dev-server', async () => {
       const files = await resolveGlob(this.bundleDir, './*.hot-update.*');
       logger.log(`clearing garbage (${files.length} files)`);
       for (const file of files) {
-        fs.unlinkSync(file);
+        await Bun.file(file).delete();
       }
       logger.log('compiling');
     });
+
     // Start reloading when it's finished
     compiler.hooks.done.tap('tgui-dev-server', async (stats) => {
       // Load source maps
@@ -72,10 +117,11 @@ class RspackCompiler {
       // Reload cache
       await reloadByondCache(this.bundleDir);
       // Notify all clients that update has happened
-      link.broadcastMessage({
+      broadcastMessage({
         type: 'hotUpdate',
       });
     });
+
     // Start watching
     logger.log('watching for changes');
     compiler.watch({}, (err, stats) => {
