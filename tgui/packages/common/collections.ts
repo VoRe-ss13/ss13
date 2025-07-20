4030bc16f805afd099a304812839e11eb0f89fa7
CHOMPStation2StaffMirrorBot <94713762+CHOMPStation2StaffMirrorBot@users.noreply.github.com>
2025-07-20 06:33:11 -0700

    [MIRROR] another update and es-toolkit

--- tgui/packages/common/collections.ts
+++ tgui/packages/common/collections.ts
@@ -4,6 +4,7 @@
  * @license MIT
  */
 
+<<<<<<< HEAD
 /**
  * Returns a range of numbers from start to end, exclusively.
  * For example, range(0, 5) will return [0, 1, 2, 3, 4].
@@ -108,6 +109,8 @@ export const zip = <T extends unknown[][]>(...arrays: T): Zip<T> => {
   return result;
 };
 
+=======
+>>>>>>> 66b765887f ([MIRROR] another update and es-toolkit (#11212))
 const binarySearch = <T, U = unknown>(
   getKey: (value: T) => U,
   collection: readonly T[],
@@ -151,50 +154,4 @@ export const binaryInsertWith = <T, U = unknown>(
   const copy = [...collection];
   copy.splice(binarySearch(getKey, collection, value), 0, value);
   return copy;
-};
-
-/**
- * This method takes a collection of items and a number, returning a collection
- * of collections, where the maximum amount of items in each is that second arg
- */
-export const paginate = <T>(collection: T[], maxPerPage: number): T[][] => {
-  const pages: T[][] = [];
-  let page: T[] = [];
-  let itemsToAdd = maxPerPage;
-
-  for (const item of collection) {
-    page.push(item);
-    itemsToAdd--;
-    if (!itemsToAdd) {
-      itemsToAdd = maxPerPage;
-      pages.push(page);
-      page = [];
-    }
-  }
-  if (page.length) {
-    pages.push(page);
-  }
-  return pages;
-};
-
-const isObject = (obj: unknown): obj is object =>
-  typeof obj === 'object' && obj !== null;
-
-// Does a deep merge of two objects. DO NOT FEED CIRCULAR OBJECTS!!
-export const deepMerge = (...objects: any[]): any => {
-  const target = {};
-  for (const object of objects) {
-    for (const key of Object.keys(object)) {
-      const targetValue = target[key];
-      const objectValue = object[key];
-      if (Array.isArray(targetValue) && Array.isArray(objectValue)) {
-        target[key] = [...targetValue, ...objectValue];
-      } else if (isObject(targetValue) && isObject(objectValue)) {
-        target[key] = deepMerge(targetValue, objectValue);
-      } else {
-        target[key] = objectValue;
-      }
-    }
-  }
-  return target;
 };
