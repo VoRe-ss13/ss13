c675bfb3be4b9f6c127e50d40fb1efba47d0ce43
CHOMPStation2StaffMirrorBot <94713762+CHOMPStation2StaffMirrorBot@users.noreply.github.com>
2025-09-21 15:42:08 -0700

    [MIRROR] dependency up

--- dependencies.sh
+++ dependencies.sh
@@ -5,7 +5,11 @@
 
 # byond version
 export BYOND_MAJOR=516
+<<<<<<< HEAD
 export BYOND_MINOR=1662
+=======
+export BYOND_MINOR=1669
+>>>>>>> a65d46aebe ([MIRROR] dependency up (#11718))
 
 # Macro Count
 export MACRO_COUNT=8
@@ -15,8 +19,14 @@ export RUST_G_VERSION=3.8.0
 
 #node version
 export NODE_VERSION_LTS=22.14.0
+<<<<<<< HEAD
 # compatiblility mode MUST work with windows 7
 export NODE_VERSION_COMPAT=20.2.0
+=======
+
+# Bun version
+export BUN_VERSION=1.2.22
+>>>>>>> a65d46aebe ([MIRROR] dependency up (#11718))
 
 # SpacemanDMM git tag
 export SPACEMAN_DMM_VERSION=suite-1.10
